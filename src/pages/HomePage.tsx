cb6a935c5146147a6b606487b699a624dd14a6fb
Shaul Kimel <shaullk600@gmail.com>
2025-09-02 16:07:50 +0300

    update: finish bulid HomePage not CSS

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import "./css/HomePage.css";
 
+import { Link } from 'react-router'
+
 import HeaderBar from "../comps/nav/HeaderBar";
 
 export default function HomePage() {
@@ -11,10 +13,11 @@ export default function HomePage() {
       </div>
 
       <div className='referringToGame'>
-
+        {/* אמור להיות בדיקה אם משתמש רשום */}
+        <Link to="/RiddleGame">link </Link>
       </div>
       <div className='referringToLogin'>
-
+        <Link to="/loginMain">link </Link>
       </div>
 
     </div>
