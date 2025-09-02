5c3b9d40853c4bb44b4e0eea11c1f3d08d965c82
Shaul Kimel <shaullk600@gmail.com>
2025-09-02 17:31:49 +0300

    update: page 'login' is started CSS style

--- src/pages/accounts/LoginRegisterPage.tsx
+++ src/pages/accounts/LoginRegisterPage.tsx
@@ -1,10 +1,48 @@
 import React from 'react'
 
+import "./LoginRegisterPage.css";
+
 export default function LoginRegisterPage() {
   return (
-    <div>
+    <div className='PageWrapper'>
 
-        
+      <div className='AuthContainer'>
+
+        <header className='AuthHeader'>
+          <h1>
+            Login
+          </h1>
+          <p> Enter user details </p>
+        </header>
+
+        <div className='enterUser'>
+
+            <div className='textLogin'>
+            <label htmlFor="email">email to account</label>
+            <input type="text" id='email' name='email' required/>
+            </div>
+
+            <div className='textLogin'>
+            <label htmlFor="username"> name </label>
+            <input type="text" id='username' name='username' required/>
+            </div>
+
+            <div className='textLogin'>
+            <label htmlFor="pass"> password </label>
+            <input type="text" id='pass' name='pass' required/>
+            </div>
+
+            <button className='btnAccess'>
+              Login
+            </button>
+        </div>
+
+
+        <footer>
+          
+        </footer>
+      </div>
+
     </div>
   )
 }
