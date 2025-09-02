3b6443a1a74193f0f5f9c2aadb405154113e1e62
Shaul Kimel <shaullk600@gmail.com>
2025-09-02 16:03:30 +0300

    addition div's

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import "./css/HomePage.css";
+
+import HeaderBar from "../comps/nav/HeaderBar";
 
 export default function HomePage() {
   return (
     <div>
-        
+      <div>
+        <HeaderBar />
+      </div>
+
+      <div className='referringToGame'>
+
+      </div>
+      <div className='referringToLogin'>
+
+      </div>
+
     </div>
   )
 }
