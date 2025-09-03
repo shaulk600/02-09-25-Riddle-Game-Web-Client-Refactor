e5b9d40807bdedc11447e7d4cd35924dccd8dd82
Shaul Kimel <shaullk600@gmail.com>
2025-09-03 11:22:59 +0300

    Adding files: Adding a footer folder inside the comps folder

---
+++ src/comps/footer/FooterPage.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function FooterPage() {
+    return (
+
+        <section id='footerPage'>
+            <div id='pContact'>
+                <h5> Contact details </h5>
+                <p>abcde@gmail.com</p>
+            </div>
+
+            <div id='pCredit'>
+                <p> ©  All rights reserved  © </p>
+                <p> shaullk600@gmail.com </p>
+            </div>
+
+        </section>
+
+    )
+}
