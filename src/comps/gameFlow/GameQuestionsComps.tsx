895bbd4d43afbcc871405e11c842fa9ed5ac9920
Shaul Kimel <shaullk600@gmail.com>
2025-09-04 12:30:12 +0300

    Adding: Adding a game interface page

---
+++ src/comps/gameFlow/GameQuestionsComps.tsx
@@ -0,0 +1,26 @@
+import "./GameQuestionsComps.css";
+
+export default function GameQuestionsComps() {
+
+    const obj = {
+        title: "",
+        anser: "",
+        
+
+    }
+    const receivingAnswer = () => {
+
+    }
+
+    return (
+        <>
+            <section>
+
+                <h1>{ }</h1>
+                <input type="text" required/>
+                <button onClick={ }></button>
+
+            </section>
+        </>
+    )
+}
